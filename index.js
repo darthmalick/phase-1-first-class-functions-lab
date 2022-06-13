9e99f47a8b044de1e1a41eab6525ecae1be0679e
mark harrison <darthmalick@gmail.com>
2022-06-12 19:57:31 -0500

    commit tree 2c0b953a73bc4047cfe82c7e00abeebd188eef0b

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = function(firstTwo){
+   return drivers.slice(0, 2);
+}
+const returnLastTwoDrivers = function(lastTwo){
+    return drivers.slice(2, 4);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier(integer){
+    return function(){
+        return integer* integer;
+    }
+}
+let fareDoubler = fare => fare*2;
+let fareTripler = fare => fare*3;
+ 
+
+function selectDifferentDrivers (drivers, selectingDrivers){
+return selectingDrivers()
+}
